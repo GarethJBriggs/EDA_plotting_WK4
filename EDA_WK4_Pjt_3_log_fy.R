90f3ff46c695681138bebf497893f5e75423d94b
Gareth Briggs <garethjamesbriggs@gmail.com>
2022-06-28 19:22:42 +0100

    Update ggplot2 syntax
    Plotting syntax changed. All levels of code now work. Needs update to facet wrap legend

--- EDA_WK4_Pjt_3_log_fy.R
+++ EDA_WK4_Pjt_3_log_fy.R
@@ -9,38 +9,29 @@ library(ggplot2)
 
         BC <-subset(NEI, fips == "24510")
         
+        ## build basic ggplot using log10 for Emissions
+        g <- ggplot(BC, aes(x = year, y = log10(Emissions))) 
+        g + geom_point( aes(color = type), alpha = 0.25, size = 2) + 
+                labs(x = "Year", y = "log10 Tons Emissions PM 2.5") + 
+                ggtitle("PM 2.5 Emissions in Baltimore City, by Type, 1999 - 2008") + 
+                facet_wrap(.~ type, scales = "free_y",) +
+                geom_smooth(method = "lm", formula = y ~ x, col = "purple") +
+                theme(plot.title = element_text(hjust = 0.5))
 
-        ## build basic ggplot using log1- for Emissions
-        g <- ggplot(BC, aes(x = year, y = log10(Emissions)))  
-        g + geom_point( aes(color = type), alpha = 0.25, size = 2)
-        g <- g + facet_wrap(.~ type, scales = "free_y") + geom_smooth(method = "lm", formula = y ~ x, col = "purple") 
-        g + labs(x = "Year", y = "log10 Tons Emissions PM 2.5")+ ggtitle("PM 2.5 Emissions in Baltimore City, by Type, 1999 - 2008")
+           ## copy to PNG file
         
-
-         
-       
-        
-       
-        
-        ## copy to PNG file
-        
-        dev.copy(png, file = "plot2.png")
+        dev.copy(png, file = "plot3.png")
         
         ## reset devices
         
         dev.off()
         
-        
-
-         g + xlab("Year") + ylab("log10 Tons Emissions PM 2.5") 
-
-      
-        + coord_cartesian(ylim = c(-6, 4))
-        
-        
-
-
-         ## log10 Emissions and prevent log of 0, which would give INF values
+       ## log10 Emissions and prevent log of 0, which would give INF values
         BC$Emissions <- ifelse (BC$Emissions!=0, log10(BC$Emissions), BC$Emissions)
         
+      
+        
+
+
+         
  
