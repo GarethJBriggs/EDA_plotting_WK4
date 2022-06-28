7a29a06e73dd3c08bb356b1469d8eefa701debb5
Gareth Briggs <garethjamesbriggs@gmail.com>
2022-06-28 14:56:56 +0100

    Update EDA_WK4_Pjt_2.R
    Title, code, and dev off checked

--- EDA_WK4_Pjt_2.R
+++ EDA_WK4_Pjt_2.R
@@ -1,20 +1,32 @@
+        plot2
+
         NEI <- readRDS("summarySCC_PM25.rds")
         SCC <- readRDS("Source_Classification_Code.rds")       
         
         ## subset for Baltimore City, Maryland identifier
         
         (BC <-subset(NEI, fips == "24510"))
-
+        
+        ## extract gross emissions and year data  
+        ## of total PM2.5 by year, to array
+        
         BCs <- tapply(BC$Emissions, BC$year, sum) 
-        BCdf <- as.data.frame(BCs)
-        BCdf$year <- rownames(BCdf)
-        rownames(BCdf) <- NULL
-        colnames(BCdf)[1] <- "total.PM2.5"
+        
+        
+        ## create a df from sum of all USA emissions array
+        BC_df <- as.data.frame(BCs)
+        
+        
+        ## process df ready for extracting values
+        
+        BC_df$year <- rownames(BC_df)
+        rownames(BC_df) <- NULL
+        colnames(BC_df)[1] <- "total.PM2.5"
 
         ## extract BCdf$year and BCdf$total.PM2.5 for plotting
 
-        xBC <- as.numeric(BCdf$year)
-        yBC <- log10(BCdf$total.PM2.5)
+        xBC <- as.numeric(BC_df$year)
+        yBC <- log10(BC_df$total.PM2.5)
         
         ## plot data
         
@@ -24,5 +36,12 @@
 
         ## fit a line
 
-        abline(lm(yBC ~ xBC), col = "33" , lwd = 2, lty = 4)
+        abline(lm(yBC ~ xBC), col = "green" , lwd = 2, lty = 4)
         
+        ## copy to PNG file
+        
+        dev.copy(png, file = "plot2.png")
+        
+        ## reset devices
+        
+        dev.off()
