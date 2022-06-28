c0b74bd21bc530eb898a3c172f0633fa11d52950
Gareth Briggs <garethjamesbriggs@gmail.com>
2022-06-28 10:56:37 +0100

    Update EDA_WK4_Pjt_6.R
    Contains highlighted redundant code for split and plotting of emissions/year by separate fips

--- EDA_WK4_Pjt_6.R
+++ EDA_WK4_Pjt_6.R
@@ -1,4 +1,4 @@
-
+library(ggplot2)
         NEI <- readRDS("summarySCC_PM25.rds")
         SCC <- readRDS("Source_Classification_Code.rds")       
 
@@ -24,23 +24,23 @@
         
         BCLA_df_mod <- BCLA_df[BCLA_df$SCC %in% mtSCC, ]
         
+        BCLA_df_mod$Emissions <- log10(BCLA_df_mod$Emissions)
+        
+        ############################################
         fips <- split(BCLA_df_mod, BCLA_df_mod$fips)
         
         df_fip_LA <- as.data.frame(fips$`06037`)
         df_fip_BC <- as.data.frame(fips$`24510`)
         
         xLA = df_fip_LA$year
-        yLA = df_fip_LA$Emissions
+        yLA = log10(df_fip_LA$Emissions)
         
         xBC = df_fip_BC$year
-        yLA = df_fip_BC$Emissions
-        
-        par(mfrow = C(1,2))
-        
-        with(BCLA_df_mod, {
-                plot(xb, yb[fips =="24510" ])
-                plot(xb, yb[fips == "06037"])
-                                           })
+        yLA = log10(df_fip_BC$Emissions)
+        #############################################
+       
+         g <- ggplot(BCLA_df_mod, aes(year, Emissions)) + geom_point(aes(color = fips), alpha = 1/3, size = 2)
+         g + facet_wrap(.~ fips, scales = "free_y") + geom_smooth(method = "lm", formula = y ~ x, col = "green")     
         
         
         
