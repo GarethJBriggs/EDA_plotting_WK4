7b9a9b08fd2cce33c8b6355af6967e804fad94ca
Gareth Briggs <garethjamesbriggs@gmail.com>
2022-06-29 16:20:38 +0100

    Abline problems
    Problems with inf and na on y axis values. Removed surplus code

--- EDA_WK4_Pjt_4.R
+++ EDA_WK4_Pjt_4.R
@@ -1,13 +1,11 @@
 library(scales)        
 
+        ##Plot 4 - problems with log/infinity
+
         NEI <- readRDS("summarySCC_PM25.rds")
         SCC <- readRDS("Source_Classification_Code.rds") 
         
         coal_EI <- grep("coal", SCC$EI.Sector, ignore.case = TRUE, value = FALSE) ## returns 99 values
-        coal_S1<- grep("coal", SCC$SCC.Level.One, ignore.case = TRUE, value = TRUE) ## returns 0 values
-        coal_S2 <- grep("coal", SCC$SCC.Level.Two, ignore.case = TRUE, value = TRUE) ## returns 0 values
-        coal_S3<- grep("coal", SCC$SCC.Level.Three, ignore.case = TRUE, value = TRUE) ## returns 181 values
-        coal_S4<- grep("coal", SCC$SCC.Level.Four, ignore.case = TRUE, value = TRUE) ## returns 126 values
         
         ## extract 99 rows containing coal combustion from SCC df
         ## gives a df of 15 cols and 99 rows 
@@ -23,13 +21,19 @@ library(scales)
         
         xc = NEI_SCC_mod$year
         
-        yc = NEI_SCC_mod$Emissions
+        yc = log10(NEI_SCC_mod$Emissions)
+        
+        table(is.infinite(yc))
          
-        yc <- ifelse(yc!=0, log10(yc), yc)
-        
-        fc = NEI_SCC_mod$fips
-        
         plot(x = xc, y= yc, pch = 19, cex = 2, lwd = 2, col = "red" )
         
         abline(lm(yc ~ xc), col = "red" , lwd = 2, lty = 4)
+        
+        ## copy to PNG file
+        
+        dev.copy(png, file = "plot4.png")
+        
+        ## reset devices
+        
+        dev.off()
      
