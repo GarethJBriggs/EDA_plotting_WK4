e9d60ba0bc649fd1c2fe5a3cfb65cffc3bb3a8e1
Gareth Briggs <garethjamesbriggs@gmail.com>
2022-07-15 11:56:38 +0100

    Update EDA_WK4_Pjt_6_v2.R
    snake_case and variable tidy

--- WK4_final/EDA_WK4_Pjt_6_v2.R
+++ WK4_final/EDA_WK4_Pjt_6_v2.R
@@ -1,16 +1,19 @@
 library(ggplot2)
 
                 
-        ## Plot 6: needs fips set as character, so facet legend shows county names not numeric ids
+        ## Plot 6
         
         NEI <- readRDS("summarySCC_PM25.rds")
         SCC <- readRDS("Source_Classification_Code.rds")       
 
         ## subset for Baltimore City, Maryland identifier & Los Angeles County
         
-        BCLA_df <-subset(NEI, fips == "24510" | fips == "06037")
+        bcla_df <-subset(NEI, fips == "24510" | fips == "06037")
         
-        BCLA_df$fips <- ifelse(BCLA_df$fips == "06037", "Los Angeles", ifelse(BCLA_df$fips == "24510" , "Baltimore" , NA))
+        ## set fips to county name
+        
+        bcla_df$fips <- ifelse(bcla_df$fips == "06037", "Los Angeles", 
+                               ifelse(bcla_df$fips == "24510" , "Baltimore" , NA))
         
         ## identify the rows contain vehicle emissions using grep
         mtor<- grep("vehicle", SCC$EI.Sector, ignore.case = TRUE, value = FALSE)
@@ -24,19 +27,20 @@ library(ggplot2)
         
         mtSCC <- mtor_SCC_df$SCC
                 
-        ## extract from BCLA_df the coal combustion rows present in mtSCC
+        ## extract from bcla_df the coal combustion rows present in mtSCC
         ## producing a df containing Baltomore and Los Angeles data associated 
         ## with motor vehicles
         
-        BCLA_df_mod <- BCLA_df[BCLA_df$SCC %in% mtSCC, ]
+        bcla_df2 <- bcla_df[bcla_df$SCC %in% mtSCC, ]
         
         ## log10 emissions in BCLA_mod df
         
-        BCLA_df_mod$Emissions <- log10(BCLA_df_mod$Emissions)
+        bcla_df2$Emissions <- log10(bcla_df2$Emissions)
         
-        ## plot ggplot, fact wrap and color for BC and LA fips, with regressions lines
+        
+        ## plot ggplot
        
-         g <- ggplot(BCLA_df_mod, aes(year, Emissions)) 
+         g <- ggplot(bcla_df2, aes(year, Emissions)) 
          g + geom_point(aes(color = fips), alpha = 1/3, size = 2) +
              facet_wrap(.~ fips, scales = "free_y") + 
              geom_smooth(method = "lm", formula = y ~ x, col = "green") +   
@@ -52,8 +56,6 @@ library(ggplot2)
          
          dev.off()
          
-         df$patients <- ifelse(df$patients==150, 100, ifelse(df$patients==350, 300, NA))
          
-         BCLA_df$fips <- ifelse(BCLA_df$fips == "06037", "Los Angeles", ifelse(BCLA_df$fips == "24510" , "Baltimore" , NA))
          
                   
